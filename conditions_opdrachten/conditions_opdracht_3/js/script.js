8787facc9ad76a9e225e24c1b22c8e65af949bbc
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-11-26 11:35:23 +0100

    commit opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,21 @@
 //javascript code
+function checkSaldo(){
+
+    var bedragDatGestortWordt = document.getElementById("geldOpname").value;
+
+    
+
+    if(bedragDatGestortWordt < 500){
+            alert("sorry, geen geld gestort");
+    }
+
+    if(bedragDatGestortWordt > 500){
+        alert("er is een salaris/bonis gestort");
+    }
+
+    if(bedragDatGestortWordt == 500){
+        alert("er is salaris gesort");
+    }
+
+
+}
