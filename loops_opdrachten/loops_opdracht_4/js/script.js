b425a33539af76dc8150e5156ff6b4a6db0181c5
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-12-04 11:25:56 +0100

    commit 2 opdracht 4 en 5

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,4 +1,4 @@
 //javascript code
-for (var teller = 0; teller <= 1000; teller++) {
+for (var teller = 1; teller <= 1000; teller += 10) {
     console.log(teller);
 }
--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+for (var teller = 1; teller <= 20; teller++) {
+    // console.log(teller);
+
+    if (teller % 2 == 0) {
+        document.getElementById("result").innerHTML += `is even ${teller} </br>`
+    } else {
+        console.log(teller + " is oneven")
+    }
+
+}
