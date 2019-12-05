54d8e5d0deb329ec6a1dea8685c592c39e922580
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-12-05 15:09:55 +0100

    commit opdracht 7 af

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,18 @@
 //javascript code
+
+for (var teller = 1; teller <= 100; teller++) {
+
+    if (teller % 3 == 0) {
+
+        document.getElementById("resultaat").innerHTML += "<div>VET</div><br>";
+    }
+
+    if (teller % 5 == 0) {
+
+        document.getElementById("resultaat").innerHTML += "<div>COOL</div<br>";
+    }
+
+    else {
+        document.getElementById("resultaat").innerHTML += teller + "<br>";
+    }
+}
