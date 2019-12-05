b59623ad21f62fbe5bbd8639679b1b3658da395f
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-12-05 17:47:18 +0100

    commit opdracht 9

--- loops_opdrachten/loops_opdracht_9/js/script.js
+++ loops_opdrachten/loops_opdracht_9/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+
+for (var teller = 1; teller <= 5; teller++) {
+    console.log(teller);
+    if (teller == 5 || teller == 5) {
+        console.warn("het einde is bereikt!")
+    }
+}
+
+for (var teller = 5; teller <= 1; teller++) {
+    console.log(teller);
+    if (teller == 1 || teller == 1) {
+        console.warn("het begin is het begin")
+    }
+}
