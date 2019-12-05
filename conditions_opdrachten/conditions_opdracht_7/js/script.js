9d4f68165fae1f6fe3affe66326f33c6bf6f04e8
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-12-05 18:00:54 +0100

    commit opdracht 7 conditions

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+
+var myOperator = "*";
+var myNumber1 = prompt("geef aub een getal op?");
+var myNumber2 = prompt("geef aub een getal op?");
+var resultaat;
+
+if (myOperator == "*") {
+    resultaat = myNumber1 * myNumber2;
+    document.getElementById("resultaat").innerHTML = resultaat;
+    console.warn("leuke som!");
+}
+
+
+
