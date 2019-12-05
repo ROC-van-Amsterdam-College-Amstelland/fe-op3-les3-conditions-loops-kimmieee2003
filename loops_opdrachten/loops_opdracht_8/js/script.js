c6f2f38e259297d84327ecd6276606d9f60119c3
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-12-05 17:26:38 +0100

    commit opdracht 8 af

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -1,1 +1,7 @@
 //javascript code
+for (var teller = 1; teller <= 50; teller++) {
+    console.log(teller);
+    if (teller == 25 || teller == 25) {
+        console.warn("het getal 25 is net geen bullseye!")
+    }
+}
